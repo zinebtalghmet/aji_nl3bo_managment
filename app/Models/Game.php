c7fc30accdf8e08275295c82a898ae40fc5b08dd
zinebtalghmet <zinebtalghmet2022@gmail.com>
2026-04-14 15:57:47 +0100

    feat: game model crud

---
+++ app/Models/Game.php
@@ -0,0 +1,79 @@
+<?php
+class Game{
+    private $conn;
+private $id;
+private $name;
+private $category_id;
+private $duration;
+private $description;
+private $difficulty;
+private $status;
+
+public function __construct ($db){
+    $db= new Database();
+    $this->conn = $db;
+}
+
+public function getId(){
+    return $this->id;
+}
+
+public function getName(){
+    return $this->name;
+}
+
+public function getCategoryId(){
+    return $this->category_id;
+}
+
+public function getDuration(){
+    return $this->duration;
+}
+public function getDescription(){
+    return $this->description;
+}
+
+public function getDifficulty() {
+        return $this->difficulty;
+}
+
+public function getStatus() {
+        return $this->status;
+}
+
+
+public function getAllGames(){
+    $sql='SELECT games.*, categories.name AS category
+    FROM games
+    LEFT JOIN categories ON games.category_id = categories.id
+    ORDER BY games.id DESC';
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}
+
+public function createGame($name,$category_id, $duration,$description,$difficulty,$status){
+$sql = 'INSERT INTO games(name,category_id,duration,description,difficulty,status)
+VALUES (?,?,?,?,?,?)';
+$stmt=$this->conn->prepare($sql);
+$stmt->execute([$name,$category_id, $duration,$description,$difficulty,$status]);
+
+}
+
+public function updateGame($id,$name,$category_id, $duration,$description,$difficulty,$status){
+    $sql='UPDATE games SET name =?,category_id=?,duration=?,description=?,difficulty=?,status=?
+    WHERE id = ?';
+$stmt = $this-conn->prepare($sql);
+$stmt->execute([$name,$category_id, $duration,$description,$difficulty,$status,$id]);
+}
+
+public function delete($id){
+    $sql ='DELETE FROM games WHERE id =?';
+    $stmt=$this->conn->prepare($sql);
+    $stmt->execute([$id]);
+}
+}
+
+
+?>
