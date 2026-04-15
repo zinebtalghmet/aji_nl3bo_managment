337f8c0ffc3f138a72852d39aa63bad652894e10
hassan_aftah <aftahhassan25@gmail.com>
2026-04-15 15:14:56 +0100

    commit_admin

--- Config/Database.php
+++ Config/Database.php
@@ -14,11 +14,7 @@ class Database{
 );
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-<<<<<<< HEAD
-        echo "connected";
-=======
-        //echo "connected";
->>>>>>> feature/authentification
+
        }catch(PDOException $e){
         die( "Erreur".$e->getMessage());
 
